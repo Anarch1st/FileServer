0de73ce51828551588e0373383215cd2683e9946
saii <saikat626@gmail.com>
2019-02-11 23:43:09 +0530

    - Added debugging - Refactored how folder's child info array is generated

--- server/fileserver.js
+++ server/fileserver.js
@@ -7,6 +7,8 @@ const sizeOf = require('image-size');
 const request = require('request');
 const archiver = require('archiver');
 const multer = require('multer');
+const util = require('util');
+const debug = require('debug')('file-server');
 const {
   exec
 } = require('child_process');
@@ -30,9 +32,12 @@ app.use(express.json());
 
 let storage = multer.diskStorage({
   destination: function (req, file, cb) {
-    cb(null, req.body.path)
+    let filePath = basePath + req.body.path;
+    debug('Upload file location %s', filePath);
+    cb(null, filePath)
   },
   filename: function (req, file, cb) {
+    debug('Upload file %s', file.originalname);
     cb(null, file.originalname);
   }
 });
@@ -42,12 +47,14 @@ let upload = multer({
 });
 
 app.post('/upload', upload.array('files'), function (req, res) {
-  res.send(req.files);
+  debug('Files uploaded');
+  res.redirect('./#' + req.body.path);
   res.end();
 });
 
 app.get('/video/*', function (req, res) {
   let filePath = basePath + decodeURI(req.url.substring(6));
+  debug('Video file path: %s', filePath);
 
   var stat;
   try {
@@ -77,12 +84,14 @@ app.get('/video/*', function (req, res) {
     };
 
     res.writeHead(206, head);
+    debug('headers: %O', head);
     file.pipe(res);
   } else {
     const head = {
       'Content-Length': fileSize,
       'Content-Type': 'video/mp4',
     }
+    debug('headers: %O', head);
     res.writeHead(200, head);
     fs.createReadStream(filePath).pipe(res);
   }
@@ -91,6 +100,7 @@ app.get('/video/*', function (req, res) {
 app.get('/getFile/*', function (req, res) {
   let filePath = basePath + decodeURI(req.url.substring(8));
 
+  debug('Get file stat: %s', filePath);
   exec('file -b -i ' + filePath, function (err, stdout, stderr) {
     const parts = stdout.split(';');
     // res.setHeader('Content-Type', parts[0]);
@@ -108,20 +118,23 @@ app.get('/get/*', function (req, res) {
   let filePath = basePath + decodeURI(req.url.substring(4));
   const pathStat = fs.lstatSync(filePath);
 
+  debug('Get file: %s', filePath);
   if (pathStat.isSymbolicLink()) {
+    debug('%s symbolicLink', filePath);
     res.send({
       type: 'link',
       data: pathStat
     });
   } else if (pathStat.isFile()) {
+    debug('%s file', filePath);
     fs.access(filePath, fs.constants.R_OK, (err) => {
       if (err) {
         res.send('Insufficient Read permission');
       } else {
         exec('file -b -i "' + filePath + '"', function (err, stdout, stderr) {
           const parts = stdout.split(';');
-          console.log(parts);
-          var sendObj = {
+
+          let sendObj = {
             'mime': parts[0],
             'encoding': parts[1].substring(1, parts[1].length - 1)
           };
@@ -130,6 +143,7 @@ app.get('/get/*', function (req, res) {
             sendObj.width = dim.width;
             sendObj.height = dim.height;
           }
+          debug('%s stats %o', filePath, sendObj);
           res.send({
             type: 'file',
             data: sendObj
@@ -140,36 +154,49 @@ app.get('/get/*', function (req, res) {
     });
   } else {
     fs.readdir(filePath, function (err, files) {
+      debug('%s folder', filePath);
       if (err) {
         delete err.path;
         res.send(err);
       } else {
-        var obj = [];
+        let obj = [];
 
-        for (var file of files) {
+        // Async all lstat operations
+        let lstat = util.promisify(fs.lstat);
 
-          if (file.substring(0, 1) === '.') {
-            continue;
-          }
-          const stat = fs.lstatSync(filePath + '/' + file);
-
-          // console.log(stat);
-          obj.push({
-            'name': file,
-            'isFile': stat.isFile(),
-            'size': stat.size,
-            'times': {
-              'birth': stat.birthtime,
-              'access': stat.atime,
-              'modify': stat.mtime,
-              'change': stat.ctime
-            },
-            'userId': stat.uid
+        let promiseArray = files
+          // Add only if not hidden file or has valid user header
+          .filter(file => (req.headers.user || file.substring(0, 1) !== '.'))
+          .map(async (file) => {
+            let stat = await lstat(filePath + '/' + file);
+            return {
+              'name': file,
+              'isFile': stat.isFile(),
+              'isLink': stat.isSymbolicLink(),
+              'size': stat.size,
+              'times': {
+                'birth': stat.birthtime,
+                'access': stat.atime,
+                'modify': stat.mtime,
+                'change': stat.ctime
+              },
+              'userId': stat.uid
+            }
+          })
+          .map(promise => {
+            // Dont let a single lstat call fail the whole. Hence wrap then in another promise
+            // which catches the error
+            return promise.catch(err => err);
           });
-        };
-        res.send({
-          type: 'folder',
-          data: obj
+
+        Promise.all(promiseArray).then(arrObj => {
+          let validResults = arrObj.filter(obj => !(obj instanceof Error));
+          res.send({
+            type: 'folder',
+            data: validResults
+          });
+
+          arrObj.filter(obj => (obj instanceof Error)).forEach(err => debug('Error lstating: %o', err))
         });
       }
     });
@@ -179,6 +206,7 @@ app.get('/get/*', function (req, res) {
 app.get('/create/*', function (req, res) {
   let filePath = basePath + decodeURI(req.url.substring(4));
 
+  debug('New folder');
   fs.mkdir(filePath, (err) => {
     if (err) {
       res.send(err);
@@ -192,6 +220,7 @@ app.get('/download/*', (req, res) => {
   let filePath = basePath + decodeURI(req.url.substring(9));
   const pathStat = fs.lstatSync(filePath);
 
+  debug('Download %s', filePath);
   if (pathStat.isSymbolicLink()) {
     res.send('Not yet supported');
   } else if (pathStat.isFile()) {
